4071b558a50d25992dc095a3aea64cbb146bdabb
sameer kumar verma <sv629688@gmail.com>
2022-08-15 14:16:44 +0530

    [DASHBOARD] drag n drop added

--- components/data/index.js
+++ components/data/index.js
@@ -1,51 +1,35 @@
 export const data = [{
-    id: 1,
-    icon: "⭕️",
     status: "open",
-    title: "Human Interest Form",
     name: "Esthet Howards",
     date: "2 days ago ",
-    rating:3,
-    content: "Fill out human interest distribution form"
 }, {
     id: 2,
-    icon: "⭕️",
-    status: "open",
+   
     name: "Esthet Howards",
     date: "2 days ago ",
     rating:3,
-    title: "Purchase present",
-    content: "Get an anniversary gift"
 }, {
     id: 3,
-    icon: "⭕️",
-    name: "Esthet Howards",
+   
     date: "2 days ago ",
     rating:3,
     status: "open",
-    title: "Invest in investments",
-    content: "Call the bank to talk about investments"
 }, {
     id: 4,
     name: "Esthet Howards",
-    date: "2 days ago ",
     rating:3,
-    icon: "⭕️",
+   
     status: "open",
-    title: "Daily reading",
-    content: "Finish reading Intro to UI/UX"
 }];
 
 export const statuses = [{
     status: "open",
-    icon: "⭕️",
-    color: "#EB5A46",
+   
     headding: "New Applied",
     noti: 10,
 }, {
     status: "in progress",
     icon: "🔆️",
-    color: "#00C2E0",
     headding: "New Applied",
     noti: 10,
 }, {
@@ -53,13 +37,11 @@ export const statuses = [{
     headding: "New Applied",
     noti: 10,
     icon: "📝",
-    color: "#C377E0"
 }, {
     status: "done",
     headding: "New Applied",
     noti: 10,
     icon: "✅",
-    color: "#3981DE"
 },
 
 
